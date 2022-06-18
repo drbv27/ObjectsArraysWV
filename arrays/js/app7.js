329cdddf98769cbf1df09088b0a759dc55a1c651
Diego Bonilla <drbv27@gmail.com>
2022-06-17 19:00:38 -0500

    eliminar elementos del array

--- arrays/js/app7.js
+++ arrays/js/app7.js
@@ -1,2 +1,36 @@
-//este es un rachivo de prueba nada mas
-//para ver que suba al github
+const carrito =[];
+
+const producto1 ={
+    nombre:"Monitor",
+    precio:200,
+}
+
+const producto2 = {
+    nombre:"Tablet 8",
+    precio:100,
+}
+
+const producto3 = {
+    nombre:"Parlantes BT",
+    precio:270,
+}
+//veamos ahora como borrar elementos
+
+
+carrito.push(producto1)
+carrito.push(producto2)
+carrito.push(producto3)
+
+//para borrar el primer elemnto de un array .shift()
+//carrito.shift();
+
+//como borramos el ultimo elemento .pop()
+
+carrito.pop()
+
+//como usar .splice() para borrar un elemneto en otra posisicion
+//carrito.splice(1,0)
+//carrito.splice(1,2)
+
+
+console.log(carrito)
